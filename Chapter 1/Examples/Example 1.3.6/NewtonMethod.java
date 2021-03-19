3572357c973555abd9c0576e36942524b90469ee
Hasan Raza <77343243+soulsaviour@users.noreply.github.com>
2021-03-20 03:14:52 +0530

    Example 1.3.6

---
+++ Chapter 1/Examples/Example 1.3.6/NewtonMethod.java
@@ -0,0 +1,13 @@
+public class NewtonMethod{
+	public static void main(String[] args){
+		double c = Double.parseDouble(args[0]);
+		double EPSILON = 1e-15;
+		double t = c;
+		while (Math.abs(t - c/t) > EPSILON * t)
+			{
+				t = (t + c/t)/2.0;
+
+			}
+			System.out.println(t);
+	}
+}
