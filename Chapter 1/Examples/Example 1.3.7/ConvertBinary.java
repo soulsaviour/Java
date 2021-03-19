16ecb20b34b5a4685917a8040b8ba2950ef4647f
Hasan Raza <77343243+soulsaviour@users.noreply.github.com>
2021-03-20 04:05:56 +0530

    Example 1.3.7

---
+++ Chapter 1/Examples/Example 1.3.7/ConvertBinary.java
@@ -0,0 +1,17 @@
+public class ConvertBinary{
+public static void main(String[] args){
+int n = Integer.parseInt(args[0]);
+int power = 1;
+while(power <= n/2)
+	{
+		power*=2;
+	}
+while(power > 0)
+	{
+		if(power > n) {System.out.print(0);}
+		else {System.out.print(1); n -= power;}
+		power/=2;
+	}
+	System.out.println();
+}
+}
